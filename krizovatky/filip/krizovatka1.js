42ee76df73ef5772f1295fb549b6aeea57adecb9
filipgriger <fb8885@gmail.com>
2021-05-09 19:35:01 +0200

    Filip krizovatka

---
+++ krizovatky/filip/krizovatka1.js
@@ -0,0 +1,101 @@
+$(function () {
+    const bluePath = anime.path('#blue-path');
+    const redPath = anime.path('#red-path');
+    const greenPath = anime.path('#green-path');
+    const purplePath = anime.path('#purple-path');
+
+    var order = [];
+
+    var blueRide = anime({
+        targets: '#blue-car',
+        translateX: bluePath('x'),
+        translateY: bluePath('y'),
+        rotate: bluePath('angle'),
+        easing: 'linear',
+        duration: 3000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#blue-car').hide();
+        }
+    });
+
+    var redRide = anime({
+        targets: '#red-car',
+        translateX: redPath('x'),
+        translateY: redPath('y'),
+        rotate: redPath('angle'),
+        easing: 'linear',
+        duration: 3000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#red-car').hide();
+        }
+    });
+
+    var greenRide = anime({
+        targets: '#green-car',
+        translateX: greenPath('x'),
+        translateY: greenPath('y'),
+        rotate: greenPath('angle'),
+        easing: 'linear',
+        duration: 3000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#green-car').hide();
+        }
+    });
+
+    var purpleRide = anime({
+        targets: '#purple-car',
+        translateX: purplePath('x'),
+        translateY: purplePath('y'),
+        rotate: purplePath('angle'),
+        easing: 'linear',
+        duration: 3000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#purple-car').hide();
+        }
+    });
+
+    $('#blue-car').click(function () {
+        blueRide.play();
+        order.push('blue');
+        result();
+    });
+
+    $('#red-car').click(function () {
+        redRide.play();
+        order.push('red');
+        result();
+    });
+
+    $('#green-car').click(function () {
+        greenRide.play();
+        order.push('green');
+        result();
+    });
+
+    $('#purple-car').click(function () {
+        purpleRide.play();
+        order.push('purple');
+        result();
+    });
+
+    function result(){
+        if (order.length === 4){
+            if ((order[1] === 'green' && order[0] === 'red')||(order[0] === 'green' && order[1] === 'red')){
+                $('.alert-success').removeClass('d-none');
+            } else {
+                $('.alert-danger').removeClass('d-none');
+            }
+        }
+    }
+
+
+
+});
---
+++ krizovatky/filip/krizovatka2.js
@@ -0,0 +1,78 @@
+$(function () {
+    const bluePath = anime.path('#blue-path');
+    const redPath = anime.path('#red-path');
+    const greenPath = anime.path('#green-path');
+
+    let order = [];
+
+    var blueRide = anime({
+        targets: '#blue-car',
+        translateX: bluePath('x'),
+        translateY: bluePath('y'),
+        rotate: bluePath('angle'),
+        easing: 'linear',
+        duration: 4000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#blue-car').hide();
+        }
+    });
+
+    var redRide = anime({
+        targets: '#red-car',
+        translateX: redPath('x'),
+        translateY: redPath('y'),
+        rotate: redPath('angle'),
+        easing: 'linear',
+        duration: 5000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#red-car').hide();
+        }
+    });
+
+    var greenRide = anime({
+        targets: '#green-car',
+        translateX: greenPath('x'),
+        translateY: greenPath('y'),
+        rotate: greenPath('angle'),
+        easing: 'linear',
+        duration: 4000,
+        loop: false,
+        autoplay: false,
+        complete: function(anim) {
+            $('#green-car').hide();
+        }
+    });
+
+
+    $('#blue-car').click(function () {
+        blueRide.play();
+        order.push('blue');
+        result();
+    });
+
+    $('#red-car').click(function () {
+        redRide.play();
+        order.push('red');
+        result();
+    });
+
+    $('#green-car').click(function () {
+        greenRide.play();
+        order.push('green');
+        result();
+    });
+
+    function result(){
+        if (order.length === 3){
+            if (order.indexOf('green') > order.indexOf('red')){
+                $('.alert-success').removeClass('d-none');
+            } else {
+                $('.alert-danger').removeClass('d-none');
+            }
+        }
+    }
+});
